b3018ad23c7731e07ab0a206c810f9a37513b8bf
Jatin Sharma <jatina9410@gmail.com>
2025-09-04 22:18:13 +0530

    hashed password

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,6 @@
 
 import mongoose, { Schema } from "mongoose";
+import bcrypt from 'bcrypt'
 
 const userSchema = new Schema({
     avatar: {
@@ -60,5 +61,10 @@ const userSchema = new Schema({
 }
 
 )
+userSchema.pre('save',async function(next){
+    if(!this.isModified('password')) return next()
+   this.password= await bcrypt.hash(this.password,10)
+    next();
+})
 
 export const User = mongoose.model('User', userSchema)
